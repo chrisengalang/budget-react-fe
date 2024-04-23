fd6243473a5b7b3622c12f1e249294d7060aac16
Chrisen Galang <chrisengalang@Chrisens-MacBook-Pro.local>
2024-04-23 19:09:13 +0800

    Initial code commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Accounts from './components/accounts/Accounts'
+
+const App = () => {
+  return (
+    <div className="container">
+      <Accounts />
+    </div>
+  )
+}
+
+export default App
