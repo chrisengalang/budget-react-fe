bccdea86824a7a640a2ab719c6f11aca53f01f28
Chrisen Galang <chrisengalang@Chrisens-MacBook-Pro.local>
2024-04-24 07:47:33 +0800

    Add account list screen with mock data

--- src/components/accounts/Accounts.jsx
+++ src/components/accounts/Accounts.jsx
@@ -1,6 +1,34 @@
+import { Card, CardBody, CardHeader, CardText, CardTitle, Col, Container, Row, Stack } from 'react-bootstrap'
+import accounts from '../../assets/mock/accounts.json'
+
 const Accounts = () => {
+
   return (
-    <div>Accounts</div>
+    <Container>
+      <h1 className='text-center my-4'>Accounts</h1>
+      <Container>
+          <Row xs={1} sm={2} md={3}>
+            {
+              accounts.map((account) => {
+                return (
+                  <Col key={account.id} className='mb-4'>
+                    <Card>
+                      <CardBody>
+                        <CardTitle>
+                          {account.account_name}
+                        </CardTitle>
+                        <CardText>
+                          {account.amount}
+                        </CardText>
+                      </CardBody>
+                    </Card>
+                  </Col>
+                )
+              })
+            }
+          </Row>
+      </Container>
+    </Container>
   )
 }
 
