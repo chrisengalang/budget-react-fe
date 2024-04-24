4f149dd2170b41110335f7d0666e031935cfaaac
Chrisen Galang <chrisengalang@Chrisens-MacBook-Pro.local>
2024-04-24 08:36:03 +0800

    Add account with mock data

--- src/components/accounts/Accounts.jsx
+++ src/components/accounts/Accounts.jsx
@@ -1,7 +1,18 @@
-import { Card, CardBody, CardHeader, CardText, CardTitle, Col, Container, Row, Stack } from 'react-bootstrap'
+import { Button, Card, CardBody, CardHeader, CardText, CardTitle, Col, Container, Row, Stack } from 'react-bootstrap'
 import accounts from '../../assets/mock/accounts.json'
+import { useEffect, useState } from 'react'
+import AddAccountModal from './AddAccountModal'
 
 const Accounts = () => {
+
+  const [accountList, setAccountList] = useState([])
+  const [showModal, setShowModal] = useState(false)
+
+  const handleShowModal = () => setShowModal(!showModal)
+
+  useEffect(() => {
+    setAccountList(accounts)
+  }, [])
 
   return (
     <Container>
@@ -9,7 +20,7 @@ const Accounts = () => {
       <Container>
           <Row xs={1} sm={2} md={3}>
             {
-              accounts.map((account) => {
+              accountList.map((account) => {
                 return (
                   <Col key={account.id} className='mb-4'>
                     <Card>
@@ -28,6 +39,10 @@ const Accounts = () => {
             }
           </Row>
       </Container>
+      <Container>
+        <Button className='mb-4 w-100' size='lg' onClick={handleShowModal}>Add Account</Button>
+      </Container>
+      <AddAccountModal handleShowModal={handleShowModal} showModal={showModal} setAccountList={setAccountList} accountList={accountList}/>
     </Container>
   )
 }
---
+++ src/components/accounts/AddAccountModal.jsx
@@ -0,0 +1,38 @@
+import { Button, FormControl, FormText, Modal, ModalBody, ModalHeader, ModalTitle, Stack } from "react-bootstrap"
+import { useState } from "react"
+
+const AddAccountModal = ({showModal, handleShowModal, setAccountList, accountList}) => {
+
+  const [accountName, setAccountName] = useState('')
+  const [initialValue, setInitialValue] = useState(0)
+
+  const handleAddAccount = () => {
+    setAccountList([...accountList, {
+      id: accountList.length + 1,
+      account_name: accountName,
+      amount: parseFloat(initialValue).toFixed(2)
+    }])
+    setAccountName('')
+    setInitialValue(0)
+    handleShowModal()
+  }
+
+  return (
+    <Modal show={showModal} onHide={handleShowModal}>
+      <ModalHeader closeButton>
+        <ModalTitle>Add Account</ModalTitle>
+      </ModalHeader>
+      <ModalBody>
+        <FormControl className="mb-4" type="text" placeholder="Account Name" onChange={(e) => setAccountName(e.target.value)} />
+        <FormControl className="mb-4" type="text" placeholder="Initial Value" onChange={(e) => setInitialValue(e.target.value)} />
+        <Stack className="justify-content-center" direction="horizontal" gap={2}>
+          <Button onClick={handleAddAccount}>Add</Button>
+          <Button onClick={handleShowModal}>Cancel</Button>
+        </Stack>
+        
+      </ModalBody>
+    </Modal>
+  )
+}
+
+export default AddAccountModal
